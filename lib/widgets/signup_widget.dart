7db790c5d9aa4a562cdf6383b67f29b55919240c
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-10-02 00:49:50 +0300

    Update signup_widget.dart

--- lib/widgets/signup_widget.dart
+++ lib/widgets/signup_widget.dart
@@ -1,18 +1,105 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 class SignUpWidget extends StatefulWidget {
   final Function() onClickedSignIn;
-  const SignUpWidget({Key? key, required  this.onClickedSignIn}) : super(key: key);
+  const SignUpWidget({Key? key, required this.onClickedSignIn})
+      : super(key: key);
 
   @override
   State<SignUpWidget> createState() => _SignUpWidgetState();
 }
 
 class _SignUpWidgetState extends State<SignUpWidget> {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
 
-    );
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) => SingleChildScrollView(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(
+              height: 60,
+            ),
+            const FlutterLogo(
+              size: 120,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const Text(
+              'Hey There, \n Welcome Back',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            TextField(
+              controller: emailController,
+              textInputAction: TextInputAction.next,
+              decoration: const InputDecoration(labelText: 'Email'),
+            ),
+            const SizedBox(
+              height: 4,
+            ),
+            TextField(
+              controller: passwordController,
+              textInputAction: TextInputAction.next,
+              decoration:const InputDecoration(labelText: 'Password'),
+              obscureText: true,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton.icon(
+              onPressed: signUp,
+              icon:const Icon(
+                Icons.arrow_forward,
+                size: 32,
+              ),
+              label:const Text(
+                'Sign Up',
+                style: TextStyle(fontSize: 24),
+              ),
+              style: ElevatedButton.styleFrom(minimumSize: Size.fromHeight(50)),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            RichText(
+                text: TextSpan(
+                    style: const TextStyle(color: Colors.white),
+                    text: 'Already have an account?  ',
+                    children: [
+                  TextSpan(
+                      recognizer: TapGestureRecognizer()
+                        ..onTap = widget.onClickedSignIn,
+                      text: 'Log In',
+                      style: TextStyle(
+                          decoration: TextDecoration.underline,
+                          color: Theme.of(context).colorScheme.secondary))
+                ]))
+          ],
+        ),
+      );
+
+  Future signUp() async {
+    showDialog(context: context, barrierDismissible: false,builder: (context)=> Center(child: const CircularProgressIndicator(),));
+    try{
+      await FirebaseAuth.instance.createUserWithEmailAndPassword(email: emailController.text.trim(), password: passwordController.text.trim());
+    } on FirebaseAuthException catch (e){
+      print(e);
+    }
   }
 }
