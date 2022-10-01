515050e98d85b5ac05b2ed86c7862d47767be2c6
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-10-01 22:22:09 +0300

    Update login_widget.dart

--- lib/widgets/login_widget.dart
+++ lib/widgets/login_widget.dart
@@ -8,10 +8,47 @@ class LoginWidget extends StatefulWidget {
 }
 
 class _LoginWidgetState extends State<LoginWidget> {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
 
-    );
-  }
+  @override
+  Widget build(BuildContext context) => SingleChildScrollView(
+        padding:const EdgeInsets.all(16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(
+              height: 40,
+            ),
+            TextField(
+              controller: emailController,
+              cursorColor: Colors.white,
+              textInputAction: TextInputAction.next,
+              decoration:const InputDecoration(labelText: 'Email'),
+            ),
+            const SizedBox(
+              height: 4,
+            ),
+            TextField(
+              controller: passwordController,
+              textInputAction: TextInputAction.done,
+              decoration:const InputDecoration(labelText: 'Password'),
+              obscureText: true,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton.icon(
+                onPressed: () {},
+                icon:const Icon(
+                  Icons.lock_open,
+                  size: 32,
+                ),
+                label:const Text(
+                  'Sign In',
+                  style: TextStyle(fontSize: 24),
+                ))
+          ],
+        ),
+      );
 }
