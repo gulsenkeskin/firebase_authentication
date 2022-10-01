1c73c9a914d17365b36913c4fd7114d4140a023a
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-10-02 01:01:30 +0300

    wrapped with form widget

--- lib/widgets/signup_widget.dart
+++ lib/widgets/signup_widget.dart
@@ -14,6 +14,7 @@ class SignUpWidget extends StatefulWidget {
 }
 
 class _SignUpWidgetState extends State<SignUpWidget> {
+  final formKey = GlobalKey<FormState>();
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
 
@@ -27,85 +28,92 @@ class _SignUpWidgetState extends State<SignUpWidget> {
   @override
   Widget build(BuildContext context) => SingleChildScrollView(
         padding: const EdgeInsets.all(16),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const SizedBox(
-              height: 60,
-            ),
-            const FlutterLogo(
-              size: 120,
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            const Text(
-              'Hey There, \n Welcome Back',
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(
-              height: 40,
-            ),
-            TextFormField(
-              controller: emailController,
-              textInputAction: TextInputAction.next,
-              decoration: const InputDecoration(labelText: 'Email'),
-              autovalidateMode: AutovalidateMode.onUserInteraction,
-              validator: (email) =>
-                  email != null && !EmailValidator.validate(email)
-                      ? 'Enter a valid email'
-                      : null,
-            ),
-            const SizedBox(
-              height: 4,
-            ),
-            TextFormField(
-              controller: passwordController,
-              textInputAction: TextInputAction.next,
-              decoration: const InputDecoration(labelText: 'Password'),
-              obscureText: true,
-              autovalidateMode: AutovalidateMode.onUserInteraction,
-              validator: (value) => value != null && value.length < 6
-                  ? 'Enter min 6 characters'
-                  : null,
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            ElevatedButton.icon(
-              onPressed: signUp,
-              icon: const Icon(
-                Icons.arrow_forward,
-                size: 32,
+        child: Form(
+          key: formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(
+                height: 60,
               ),
-              label: const Text(
-                'Sign Up',
-                style: TextStyle(fontSize: 24),
+              const FlutterLogo(
+                size: 120,
               ),
-              style: ElevatedButton.styleFrom(minimumSize: Size.fromHeight(50)),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            RichText(
-                text: TextSpan(
-                    style: const TextStyle(color: Colors.white),
-                    text: 'Already have an account?  ',
-                    children: [
-                  TextSpan(
-                      recognizer: TapGestureRecognizer()
-                        ..onTap = widget.onClickedSignIn,
-                      text: 'Log In',
-                      style: TextStyle(
-                          decoration: TextDecoration.underline,
-                          color: Theme.of(context).colorScheme.secondary))
-                ]))
-          ],
+              const SizedBox(
+                height: 20,
+              ),
+              const Text(
+                'Hey There, \n Welcome Back',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              TextFormField(
+                controller: emailController,
+                textInputAction: TextInputAction.next,
+                decoration: const InputDecoration(labelText: 'Email'),
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                validator: (email) =>
+                    email != null && !EmailValidator.validate(email)
+                        ? 'Enter a valid email'
+                        : null,
+              ),
+              const SizedBox(
+                height: 4,
+              ),
+              TextFormField(
+                controller: passwordController,
+                textInputAction: TextInputAction.next,
+                decoration: const InputDecoration(labelText: 'Password'),
+                obscureText: true,
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                validator: (value) => value != null && value.length < 6
+                    ? 'Enter min 6 characters'
+                    : null,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              ElevatedButton.icon(
+                onPressed: signUp,
+                icon: const Icon(
+                  Icons.arrow_forward,
+                  size: 32,
+                ),
+                label: const Text(
+                  'Sign Up',
+                  style: TextStyle(fontSize: 24),
+                ),
+                style:
+                    ElevatedButton.styleFrom(minimumSize: Size.fromHeight(50)),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              RichText(
+                  text: TextSpan(
+                      style: const TextStyle(color: Colors.white),
+                      text: 'Already have an account?  ',
+                      children: [
+                    TextSpan(
+                        recognizer: TapGestureRecognizer()
+                          ..onTap = widget.onClickedSignIn,
+                        text: 'Log In',
+                        style: TextStyle(
+                            decoration: TextDecoration.underline,
+                            color: Theme.of(context).colorScheme.secondary))
+                  ]))
+            ],
+          ),
         ),
       );
 
   Future signUp() async {
+    final isValid=formKey.currentState!.validate();
+    if(!isValid) return;
+    
     showDialog(
         context: context,
         barrierDismissible: false,
