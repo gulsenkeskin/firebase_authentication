b627d33745583c8bb2d55a37d6b0b1956c9a96f9
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-10-01 23:57:24 +0300

    Update home_page.dart

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -6,10 +6,30 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final user= FirebaseAuth.instance.currentUser!;
+    //giriş yapan kullanıcının  bilgilerine erişmek için
+    final user = FirebaseAuth.instance.currentUser!;
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(32),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Signed In as',
+              style: TextStyle(fontSize: 16),
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            Text(
+              user.email!,
+              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            )
+          ],
+        ),
       ),
     );
   }
