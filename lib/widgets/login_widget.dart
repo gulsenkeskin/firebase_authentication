610228ff70f292d0a1af0d974ed6ab592017a1ab
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-10-01 22:11:59 +0300

    added login widget

---
+++ lib/widgets/login_widget.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class LoginWidget extends StatefulWidget {
+  const LoginWidget({Key? key}) : super(key: key);
+
+  @override
+  State<LoginWidget> createState() => _LoginWidgetState();
+}
+
+class _LoginWidgetState extends State<LoginWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
+}
