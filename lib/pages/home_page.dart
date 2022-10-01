aea51aa877e93bf806b0694bc414949c2ec769ec
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-10-01 23:53:40 +0300

    Update home_page.dart

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -5,8 +6,11 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-
+    final user= FirebaseAuth.instance.currentUser!;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
     );
   }
 }
