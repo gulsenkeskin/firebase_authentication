4c486bb3c1f8c8f9e0a6ab6f6721a570da8be8d7
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-10-02 10:58:58 +0300

    e posta doğrulama

--- lib/pages/verify_email_page.dart
+++ lib/pages/verify_email_page.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_authentication/pages/home_page.dart';
+import 'package:firebase_authentication/utils/utils.dart';
 import 'package:flutter/material.dart';
 
 class VerifyEmailPage extends StatefulWidget {
@@ -10,6 +14,49 @@ class VerifyEmailPage extends StatefulWidget {
 
 class _VerifyEmailPageState extends State<VerifyEmailPage> {
   bool isEmailVerified = false;
+  Timer? timer;
+
+  @override
+  void initState() {
+    super.initState();
+    isEmailVerified = FirebaseAuth.instance.currentUser!.emailVerified;
+    if (!isEmailVerified) {
+      //eposta doğrulanmamışsa bir doğrulama epostası gönderir
+      sendVerificationEmail();
+
+      //3 saniyede bir epostanın doğrulanıp doğrulanmadığını kontrol eder
+      timer = Timer.periodic(
+        const Duration(seconds: 3),
+        (_) => checkEmailVerified(),
+      );
+    }
+  }
+
+  @override
+  void dispose() {
+    timer?.cancel();
+    super.dispose();
+  }
+
+  Future checkEmailVerified() async {
+    //kullanıcıyı yeniden yükler
+    await FirebaseAuth.instance.currentUser!.reload();
+    setState(() {
+      isEmailVerified = FirebaseAuth.instance.currentUser!.emailVerified;
+    });
+
+    if (isEmailVerified) timer?.cancel();
+  }
+
+  Future sendVerificationEmail() async {
+    try {
+      final user = FirebaseAuth.instance.currentUser!;
+      await user.sendEmailVerification();
+    } catch (e) {
+      Utils.showSnackBar(e.toString());
+    }
+  }
+
   @override
   Widget build(BuildContext context) => isEmailVerified
       ? const HomePage()
