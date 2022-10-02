3d72c85ad8e99008a3e59ea8e2a358b97ee6023b
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-10-02 18:02:58 +0300

    Update input_theme.dart

--- lib/common/input_theme.dart
+++ lib/common/input_theme.dart
@@ -1,7 +1,23 @@
 import 'dart:ui';
 
+import 'package:flutter/material.dart';
+
 class CustomInputTheme {
   TextStyle _buildTextStyle(Color color, {double size = 16.0}) {
     return TextStyle(color: color, fontSize: size);
   }
+
+  OutlineInputBorder _buildBorder(Color color) {
+    return OutlineInputBorder(
+        borderRadius: BorderRadius.all(Radius.circular(20)),
+        borderSide: BorderSide(color: color, width: 1.0));
+  }
+
+  InputDecorationTheme theme()=> InputDecorationTheme(
+    contentPadding: EdgeInsets.all(16),
+    isDense: true
+  );
+
+
+
 }
