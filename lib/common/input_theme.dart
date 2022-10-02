e85021f0376723c44f4016d2dc9657676c840dc1
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-10-02 18:19:54 +0300

    Update input_theme.dart

--- lib/common/input_theme.dart
+++ lib/common/input_theme.dart
@@ -13,11 +13,25 @@ class CustomInputTheme {
         borderSide: BorderSide(color: color, width: 1.0));
   }
 
-  InputDecorationTheme theme()=> InputDecorationTheme(
-    contentPadding: EdgeInsets.all(16),
-    isDense: true
-  );
+  InputDecorationTheme theme() => InputDecorationTheme(
+        contentPadding: EdgeInsets.all(16),
+        isDense: true,
+        //label'ı her zaman yukarıya koyar :D
+        floatingLabelBehavior: FloatingLabelBehavior.always,
 
-
-
+        constraints: BoxConstraints(maxWidth: 150),
+        enabledBorder: _buildBorder(Colors.grey[600]!),
+        errorBorder: _buildBorder(Colors.red),
+        focusedErrorBorder: _buildBorder(Colors.redAccent),
+        focusedBorder: _buildBorder(Colors.green),
+        disabledBorder: _buildBorder(Colors.grey[400]!),
+        suffixStyle: _buildTextStyle(Colors.black),
+        counterStyle: _buildTextStyle(Colors.grey, size: 12.0),
+        floatingLabelStyle: _buildTextStyle(Colors.black),
+        errorStyle: _buildTextStyle(Colors.red, size: 12.0),
+        helperStyle: _buildTextStyle(Colors.black, size: 12.0),
+        hintStyle: _buildTextStyle(Colors.grey),
+        labelStyle: _buildTextStyle(Colors.black),
+        prefixStyle: _buildTextStyle(Colors.black),
+      );
 }
