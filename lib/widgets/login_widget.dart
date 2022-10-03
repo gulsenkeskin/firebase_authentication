ed8358e384dd36706a257357a90ada40f271fac8
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-10-03 11:17:56 +0300

    Update login_widget.dart

--- lib/widgets/login_widget.dart
+++ lib/widgets/login_widget.dart
@@ -29,79 +29,81 @@ class _LoginWidgetState extends State<LoginWidget> {
   }
 
   @override
-  Widget build(BuildContext context) => SingleChildScrollView(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const SizedBox(
-              height: 40,
-            ),
-            EmailField(controller: emailController),
-            // TextField(
-            //   controller: emailController,
-            //   // cursorColor: Colors.white,
-            //   textInputAction: TextInputAction.next,
-            //   decoration: const InputDecoration(labelText: 'Email'),
-            // ),
-            const SizedBox(
-              height: 4,
-            ),
-            PasswordField(controller: passwordController,textInputAction: TextInputAction.done,),
-            // TextField(
-            //   controller: passwordController,
-            //   textInputAction: TextInputAction.done,
-            //   decoration: const InputDecoration(
-            //     labelText: 'Password',
-            //   ),
-            //   obscureText: true,
-            // ),
-            const SizedBox(
-              height: 20,
-            ),
-            ElevatedButton.icon(
-                style: ElevatedButton.styleFrom(
-                  minimumSize: const Size.fromHeight(50),
-                ),
-                onPressed: signIn,
-                icon: const Icon(
-                  Icons.lock_open,
-                  size: 28,
-                ),
-                label: const Text(
-                  'Sign In',
-                  style: TextStyle(fontSize: 24),
-                )),
-            const SizedBox(
-              height: 24,
-            ),
-            GestureDetector(
-              child: Text(
-                'Forgot Password?',
-                style: TextStyle(
-                    decoration: TextDecoration.underline,
-                    color: Theme.of(context).colorScheme.secondary,
-                    fontSize: 20),
+  Widget build(BuildContext context) => Center(
+    child: SingleChildScrollView(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(
+                height: 40,
               ),
-              onTap: () => Navigator.of(context).push(MaterialPageRoute(
-                  builder: (context) => ForgotPasswordPage())),
-            ),
-            RichText(
-                text: TextSpan(
-                   style:const TextStyle(color: Colors.black),
-                    text: 'No account?  ',
-                    children: [
-                  TextSpan(
-                      recognizer: TapGestureRecognizer()
-                        ..onTap = widget.onClickedSignUp,
-                      text: 'Sign Up',
-                      style: TextStyle(
-                          decoration: TextDecoration.underline,
-                          color: Theme.of(context).colorScheme.secondary))
-                ]))
-          ],
+              EmailField(controller: emailController),
+              // TextField(
+              //   controller: emailController,
+              //   // cursorColor: Colors.white,
+              //   textInputAction: TextInputAction.next,
+              //   decoration: const InputDecoration(labelText: 'Email'),
+              // ),
+              const SizedBox(
+                height: 4,
+              ),
+              PasswordField(controller: passwordController,textInputAction: TextInputAction.done,),
+              // TextField(
+              //   controller: passwordController,
+              //   textInputAction: TextInputAction.done,
+              //   decoration: const InputDecoration(
+              //     labelText: 'Password',
+              //   ),
+              //   obscureText: true,
+              // ),
+              const SizedBox(
+                height: 20,
+              ),
+              ElevatedButton.icon(
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: const Size.fromHeight(50),
+                  ),
+                  onPressed: signIn,
+                  icon: const Icon(
+                    Icons.lock_open,
+                    size: 28,
+                  ),
+                  label: const Text(
+                    'Sign In',
+                    style: TextStyle(fontSize: 24),
+                  )),
+              const SizedBox(
+                height: 24,
+              ),
+              GestureDetector(
+                child: Text(
+                  'Forgot Password?',
+                  style: TextStyle(
+                      decoration: TextDecoration.underline,
+                      color: Theme.of(context).colorScheme.secondary,
+                      fontSize: 20),
+                ),
+                onTap: () => Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => ForgotPasswordPage())),
+              ),
+              RichText(
+                  text: TextSpan(
+                     style:const TextStyle(color: Colors.black),
+                      text: 'No account?  ',
+                      children: [
+                    TextSpan(
+                        recognizer: TapGestureRecognizer()
+                          ..onTap = widget.onClickedSignUp,
+                        text: 'Sign Up',
+                        style: TextStyle(
+                            decoration: TextDecoration.underline,
+                            color: Theme.of(context).colorScheme.secondary))
+                  ]))
+            ],
+          ),
         ),
-      );
+  );
 
   Future signIn() async {
     showDialog(
